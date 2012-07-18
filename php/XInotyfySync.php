bd716fa0205b07796ddaf8d104cbd26dd42baba5
bigmonster <chopins.xiao@gmail.com>
2012-07-18 16:35:08 +0800

    修改

---
+++ php/XInotyfySync.php
@@ -0,0 +1,167 @@
+<?php
+class XInotifySync {
+    public $inotify_instance = null;
+    public $watch_descriptor = array();
+    public $pending_event = 0;
+    public $watch_list_conf_wd = null;
+    public $cfg = null;
+    public $inotify_sock = null;
+    public $sync_sock = null;
+    public $max_sync_process_num = 5;
+    public function __construct() {
+        daemon();
+        $this->load_cfg();
+        $ips = stream_socket_pair(STREAM_RF_UNIX, STREAM_SOCK_STREAM,STREAM_IPPROTO_IP);
+        //fork inotify process
+        $pid = pcntl_fork();
+        if($pid == -1) throw new XException('fork inotify process failure');
+        if($pid == 0) {
+            $this->inotify_sock = $ips[0];
+            fclose($ips[1]);
+            $this->create_inotify_instance();
+            $watch_list_conf_file = $this->cfg->watch_list_conf;
+            $this->watch_list_conf_wd = $this->watch($watch_list_conf_file);
+            $this->watch_loop();
+            exit(0);
+        }
+        //fork sync process
+        $pid = pcntl_fork();
+        if($pid == -1) throw new XException('fork sync process failure');
+        if($pid == 0) {
+            $this->sync_sock = $ips[1];
+            fclose($ips[0]);
+            $this->sync_master_process_loop();
+            exit(0);
+        }
+        pcntl_wait($status);
+    }
+    public function load_cfg($_CFG = null) {
+        if($_CFG == null) {
+            global $_CFG;
+        }
+        $this->cfg = $_CFG->inotify;
+    }
+    public function create_inotify_instance() {
+        $this->inotify_instance = inotify_init();
+    }
+    public function watch($path,$ip,$port, $tpath) {
+        $path = trim($path);
+        $wd = inotify_add_watch($this->inotify_instance,$path,
+                            IN_DELETE_SELF|IN_MOVE_SELF|IN_MODIFY|IN_CREATE|IN_MOVE|IN_DELETE);
+        $this->watch_descriptor[$wd]['wd'] = $wd;
+        $this->watch_descriptor[$wd]['path'] = $path;
+        $this->watch_descriptor[$wd]['target_ip'] = $ip;
+        $this->watch_descriptor[$wd]['target_port'] = $port;
+        $this->watch_descriptor[$wd]['target_path'] = $tpath;
+        return $wd;
+    }
+    public function rm($wd) {
+        inotify_rm_watch($this->inotify_instance, $wd);
+        unset($this->watch_descriptor[$wd]);
+    }
+    public function rm_all_watch() {
+        foreach($this->watch_descriptor as $wd => $path) {
+            $this->rm($wd);
+        }
+    }
+    public function queue() {
+        $this->pending_event = inotify_queue_len($this->inotify_instance);
+    }
+    public function get() {
+        return inotify_read($this->inotify_instance);
+    }
+    public function add_form_array($file_list) {
+        if(!is_array($file_list)) return;
+        foreach($file_list as $file) {
+            $this->watch($file['path'],$file['ip'],$file['port'],$file['tpath']);
+        }
+    }
+    public function sync_master_process_loop() {
+        stream_set_blocking($this->sync_sock, 1);
+        while(1) {
+            $acp = stream_socket_accept($this->sync_sock, 0);
+            $str = stream_socket_recvfrom($acp,2048);
+            $this->sync_file($str);
+            fclose($acp);
+        }
+    }
+    public function sync_file($str) {
+        list($this->watch_descriptor,$change_list) = unserialize($str);
+        $file_num = count($change_list['C']) + count($change_list['U']);
+        $this->exec_sync_cmd($change_list['D'],$change_list['M']);
+        if($file_num >= $this->max_sync_process_num) {
+            $max_num = $this->max_sync_process_num;
+        } else {
+            $max_num = $file_num;
+        }
+        for($i=0;$i<$file_num;$i++) {
+            $this->exec_sync_send_file($change_list[$i];
+        }
+    }
+    public function exec_sync_cmd($delete, $move) {
+    }
+    public function notify_file($watch_info, $change) {
+        $change_str = serialize(array($watch_info,$change));
+        stream_socket_sendto($this->inotify_sock,$change_str);
+    }
+    public function add_form_file($file) {
+        if(!is_file($file)) return;
+        $fh = SplFileObject($file,'r');
+        while(!$fh->eof()) {
+            $conf_line = $fh->fgets();
+            list($ini,) = explode('#',$conf_line,2);
+            $ini = trim($ini);
+            if(empty($ini)) continue;
+            list($path, $ip, $port, $tpath) = explode(':',$ini);
+            $this->watch($path,$ip, $port,$tpath);
+        }
+    }
+    public function reload_watch_list($ev_info) {
+        $this->rm_all_watch();
+        if($ev_info['mask'] & IN_DELETE_SELF ||
+                $ev_info['mask'] & IN_MOVE_SELF) {
+            return;
+        }
+        $this->add_form_file($this->cfg->watch_list_conf);
+    }
+    public function loop() {
+        while(true) {
+            stream_set_blocking($this->inotify_instance,1);
+            $events = $this->get();
+            $change = array();
+            $change['C'] = array();
+            $change['M'] = array();
+            $change['D'] = array();
+            $change['U'] = array();
+            foreach($events as $ev => $ev_info) {
+                switch(true) {
+                    case ($ev_info['wd'] & $this->watch_list_conf_wd):
+                        $this->reload_watch_list($ev_info);
+                    break;
+                    case $ev_info['mask'] & IN_CREATE: //创建
+                        $change['C'][] = $ev_info['name'];
+                    break;
+                    case $ev_info['mask'] & IN_MODIFY:  //修改
+                        $change['U'][] = $ev_info;
+                    break;
+                    case $ev_info['mask'] & IN_MOVE: //移动
+                        $change['M'][] = $ev_info['name'];
+                    break;
+                    case $ev_info['mask'] & IN_DELETE:  //删除
+                        $change['D'][] = $ev_info['name'];
+                    break;
+                    case $ev_info['mask'] & IN_DELETE_SELF:  //监视文件夹删除
+                    break;
+                    case $ev_info['mask'] & IN_MOVE_SELF: //监视文件夹移动
+                    break;
+                    default:
+                    break;
+                }
+            }
+            $this->sendfile($this->watch_descriptor, $change);
+        }
+    }
+    public function __destruct() {
+        fclose($this->inotify_instance);
+    }
+}
--- php/XSVNClient.php
+++ php/XSVNClient.php
@@ -3,15 +3,53 @@ class XSVNClient {
     private $socket = '';
     private $errno;
     private $errstr;
+    private $server_data_dir;
+    private $server_url;
+    private $local_dir;
+    private $repos_name = null;
     public function __construct() {
         $this->load_cfg();
         dl_extension('svn', 'svn_checkout');
-        $this->deamon();
+   //     $this->deamon();
+    }
+    public function set_repos_name($name) {
+        $this->repos_name = $name;
+    }
+    public function repos_list() {
+        return scandir($this->server_data_dir);
+    }
+    public function ls($dir = '/') {
+        return svn_ls($this->server_url.'/'.$this->repos_name.$dir);
+    }
+    public function checkout() {
+        return svn_checkout($this->server_url.$this->repos_name, 
+                        $this->local_dir.'/'.$this->repos_name);
+    }
+    public function worker_revision() {
+        $info = svn_info($this->local_dir.'/'.$this->repos_name, false);
+        return $info[0];
+    }
+    public function update($filepath) {
+        return = svn_update($this->local_dir.'/'.$this->repos_name.$filepath);
+    }
+    public function update_all() {
+        return svn_update($this->local_dir.'/'.$this->repos_name);
+    }
+    public function status() {
+        return svn_status($this->local_dir.'/'.$this->repos_name);
+    }
+    public function logs($path = '/') {
+        return svn_log($this->server_url.'/'.$this->repos_name.$path);
     }
     private function load_cfg($_CFG=null) {
         if($_CFG === null) {
             global $_CFG;
         }
+        $this->server_url = $_CFG->svn->server_url;
+        $this->server_data_dir = $_CFG->svn->server_data_dir;
+        $this->local_dir = $_CFG->svn->local_dir;
+
+        return;
         if(strtolower($_CFG->svn->protocol) != 'unix') throw new XException('XSVNClient deamon only support unix:// transports');
         $this->run_dir = empty($_CFG->run_dir) ?
             __X_APP_ROOT__."/{$_CFG->data_dir_name}/run": $_CFG->run_dir;
