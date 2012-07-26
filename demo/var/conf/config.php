73ada076b83ed91d1fecfe29e97ce173e8ee46e0
bigmonster <chopins.xiao@gmail.com>
2012-07-26 16:46:13 +0800

    change the configure file format form php to ini

--- demo/var/conf/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_CFG->db->password = '112211';
-$_CFG->db->name = 'x_company';
-$_CFG->tpl->common_tpldata->site_name = 'Dopush';//站点名字
-$_CFG->web->document_root = '/home/chopin/Code/toknot/demo/';
-$_CFG->tpl->static_dir_name = '/home/chopin/Code/toknot/demo/static';
