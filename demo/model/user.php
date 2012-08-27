a9e9b9b3144bc4647de5c3e8dfb7ba51abf015e3
bigmonster <chopins.xiao@gmail.com>
2012-08-27 17:45:27 +0800

    update demo

--- demo/model/user.php
+++ demo/model/user.php
@@ -5,11 +5,11 @@ class userModel extends XDataModel {
         $this->connect_database($admin_table);
     }
     public function get_user_info($username) {
-        $user_info = $this->DB->admin_table->get($username);
+        $user_info = $this->DB('admin_table')->get($username);
         return $user_info;
     }
     public function add_user($user_info) {
         $user_info['password'] = md5($user_info['password'],true);
-        return $ret = $this->DB->admin_table->add($user_info['username'], $user_info);
+        return $ret = $this->DB('admin_table')->add($user_info['username'], $user_info);
     }
 }
