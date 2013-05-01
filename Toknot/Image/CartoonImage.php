33e94ab120a6fb76e85e8fc466ea4d3e549bd8d5
chopin xiao <chopins.xiao@gmail.com>
2013-05-01 18:21:52 +0800

    update

---
+++ Toknot/Image/CartoonImage.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Toknot (http://toknot.com)
+ *
+ * @copyright  Copyright (c) 2011 - 2013 Toknot.com
+ * @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ * @link       https://github.com/chopins/toknot
+ */
+
+namespace Toknot\Image\CartoonImage;
+
+class CartoonImage {
+
+    private $originImg = null;
+    private $outImg = null;
+    private $with = 0;
+    private $height = 0;
+
+    public function __construct($src, $des) {
+        $this->orginImg = imagecreatefromjpeg($src);
+        
+        $this->outImg = $des;
+    }
+
+    public function toBack() {
+        imagecopymergegray($this->originImg, $this->outImg,
+                            0,0,0,0,50);
+    }
+
+}
+
+?>
