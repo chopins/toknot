95169883c9ab85d2a40a09ea1177512275a3ad43
chopin <chopins.xiao@gmail.com>
2013-05-15 19:12:34 +0800

    update

---
+++ Toknot/Db/DbTableColumn.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+* Toknot (http://toknot.com)
+*
+* @copyright Copyright (c) 2011 - 2013 Toknot.com
+* @license http://opensource.org/licenses/bsd-license.php New BSD License
+* @link https://github.com/chopins/toknot
+*/
+
+namespace Toknot\Db;
+
+use Toknot\Db\DbTableObject;
+use Toknot\Di\Object;
+use Toknot\Exception\StandardException;
+
+class DbTableColumn extends Object{
+    private $columnName = null;
+    private $tableObject = null;
+    public $alias = null;
+    public function __construct($columnName, DbTableObject &$tableObject) {
+        $this->columnName = $columnName;
+        $this->tableObject = $tableObject;
+    }
+    public function __get($name) {
+        if(isset($this->$name)) {
+            return $this->$name;
+        }
+        throw new StandardException("bad call property of {$this->columnName}::{$name}");
+    }
+
+    public function __toString() {
+        return $this->columnName;
+    }
+}
---
+++ Toknot/Db/Exception/DatabaseConfigException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Toknot (http://toknot.com)
+ *
+ * @copyright  Copyright (c) 2011 - 2013 Toknot.com
+ * @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ * @link       https://github.com/chopins/toknot
+ */
+
+namespace Toknot\Db\Exception;
+use Toknot\Exception\StandardException;
+
+class DatabaseConfigException extends StandardException {
+    public function __construct($configParam) {
+        $message = "Must be set $configParam of database connect in ini of database sections,<br />\n";
+        $message .= 'database of options has dsn,username,password,dirverOptions(which is array type),prefix(which is table name prefix and option)';
+        parent::__construct($message);
+    }
+}
+
+?>
