478a14ec2824cf13ae8b0d60bf8f17a527780b5e
chopin xiao <chopins.xiao@gmail.com>
2013-06-17 00:57:23 +0800

    -aupdate

--- Toknot/Admin/Static/js/toknot.js
+++ Toknot/Admin/Static/js/toknot.js
@@ -2,7 +2,7 @@ if (typeof TK == 'undefined') {
     String.prototype.isEmail = function() {
         return /^([a-z0-9+_]|\-|\.)+@(([a-z0-9_]|\-)+\.)+[a-z]{2,6}$/.test(this);
     };
-    String.prototype.isMoblie = function() {
+    String.prototype.isCNMoblie = function() {
         return /^(13|15|18)\d{9}$/i.test(this);
     };
     String.prototype.trim = function() {
@@ -33,182 +33,8 @@ if (typeof TK == 'undefined') {
             DOCUMENT_NODE: 9,
             DOCUMENT_FRAGMENT_NODE: 11
         };
-    };
-    if (typeof(JSON) === 'undefined') {
-        var JSON;
-        if (!JSON) {
-            JSON = {};
-        }
-        (function() {
-            "use strict";
-            function f(n) {
-                return n < 10 ? '0' + n : n;
-            }
-            if (typeof Date.prototype.toJSON !== 'function') {
-                Date.prototype.toJSON = function(key) {
-                    return isFinite(this.valueOf()) ?
-                            this.getUTCFullYear() + '-' +
-                            f(this.getUTCMonth() + 1) + '-' +
-                            f(this.getUTCDate()) + 'T' +
-                            f(this.getUTCHours()) + ':' +
-                            f(this.getUTCMinutes()) + ':' +
-                            f(this.getUTCSeconds()) + 'Z' : null;
-                };
-                String.prototype.toJSON =
-                        Number.prototype.toJSON =
-                        Boolean.prototype.toJSON = function(key) {
-                    return this.valueOf();
-                };
-            }
-            var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-                    escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-                    gap,
-                    indent,
-                    meta = {'\b': '\\b',
-                '\t': '\\t',
-                '\n': '\\n',
-                '\f': '\\f',
-                '\r': '\\r',
-                '"': '\\"',
-                '\\': '\\\\'
-            },
-            rep;
-            function quote(string) {
-                escapable.lastIndex = 0;
-                return escapable.test(string) ? '"' + string.replace(escapable, function(a) {
-                    var c = meta[a];
-                    return typeof c === 'string' ? c :
-                            '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-                }) + '"' : '"' + string + '"';
-            }
-            function str(key, holder) {
-                var i, k, v, length,
-                        mind = gap,
-                        partial,
-                        value = holder[key];
-                if (value && typeof value === 'object' &&
-                        typeof value.toJSON === 'function') {
-                    value = value.toJSON(key);
-                }
-                if (typeof rep === 'function') {
-                    value = rep.call(holder, key, value);
-                }
-                switch (typeof value) {
-                    case 'string':
-                        return quote(value);
-                    case 'number':
-                        return isFinite(value) ? String(value) : 'null';
-                    case 'boolean':
-                    case 'null':
-                        return String(value);
-                    case 'object':
-                        if (!value) {
-                            return 'null';
-                        }
-                        gap += indent;
-                        partial = [];
-                        if (Object.prototype.toString.apply(value) === '[object Array]') {
-                            length = value.length;
-                            for (i = 0; i < length; i += 1) {
-                                partial[i] = str(i, value) || 'null';
-                            }
-                            v = partial.length === 0 ? '[]' : gap ?
-                                    '[\n' + gap + partial.join(',\n' + gap) + '\n' + mind + ']' :
-                                    '[' + partial.join(',') + ']';
-                            gap = mind;
-                            return v;
-                        }
-                        if (rep && typeof rep === 'object') {
-                            length = rep.length;
-                            for (i = 0; i < length; i += 1) {
-                                k = rep[i];
-                                if (typeof k === 'string') {
-                                    v = str(k, value);
-                                    if (v) {
-                                        partial.push(quote(k) + (gap ? ': ' : ':') + v);
-                                    }
-                                }
-                            }
-                        } else {
-                            for (k in value) {
-                                if (Object.prototype.hasOwnProperty.call(value, k)) {
-                                    v = str(k, value);
-                                    if (v) {
-                                        partial.push(quote(k) + (gap ? ': ' : ':') + v);
-                                    }
-                                }
-                            }
-                        }
-                        v = partial.length === 0 ? '{}' : gap ?
-                                '{\n' + gap + partial.join(',\n' + gap) + '\n' + mind + '}' :
-                                '{' + partial.join(',') + '}';
-                        gap = mind;
-                        return v;
-                }
-            }
-            if (typeof JSON.stringify !== 'function') {
-                JSON.stringify = function(value, replacer, space) {
-                    var i;
-                    gap = '';
-                    indent = '';
-                    if (typeof space === 'number') {
-                        for (i = 0; i < space; i += 1) {
-                            indent += ' ';
-                        }
-                    } else if (typeof space === 'string') {
-                        indent = space;
-                    }
-                    rep = replacer;
-                    if (replacer && typeof replacer !== 'function' &&
-                            (typeof replacer !== 'object' ||
-                                    typeof replacer.length !== 'number')) {
-                        throw new Error('JSON.stringify');
-                    }
-                    return str('', {'': value});
-                };
-            }
-            if (typeof JSON.parse !== 'function') {
-                JSON.parse = function(text, reviver) {
-                    var j;
-                    function walk(holder, key) {
-                        var k, v, value = holder[key];
-                        if (value && typeof value === 'object') {
-                            for (k in value) {
-                                if (Object.prototype.hasOwnProperty.call(value, k)) {
-                                    v = walk(value, k);
-                                    if (v !== undefined) {
-                                        value[k] = v;
-                                    } else {
-                                        delete value[k];
-                                    }
-                                }
-                            }
-                        }
-                        return reviver.call(holder, key, value);
-                    }
-                    text = String(text);
-                    cx.lastIndex = 0;
-                    if (cx.test(text)) {
-                        text = text.replace(cx, function(a) {
-                            return '\\u' +
-                                    ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-                        });
-                    }
-                    if (/^[\],:{}\s]*$/
-                            .test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
-                            .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
-                            .replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-                        j = eval('(' + text + ')');
-                        return typeof reviver === 'function' ?
-                                walk({'': j}, '') : j;
-                    }
-                    throw new SyntaxError('JSON.parse');
-                };
-            }
-        }());
     }
     ;
-
     if (typeof console == 'undefined') {
         console = {};
         console.warn = function(str) {
@@ -1996,23 +1822,23 @@ if (typeof TK == 'undefined') {
         time: function() {
             return new Date().getTime();
         },
-        preZero : function(num) {
-            return num < 10 ? num : '0'+num;
+        preZero: function(num) {
+            return num < 10 ? num : '0' + num;
         },
-        date : function() {
+        date: function() {
             var date = new Date();
             var month = TK.preZero(date.getMonth() + 1);
             var date = TK.preZero(date.getDate());
             var hours = TK.preZero(date.getHours());
             var minutes = TK.preZero(date.getMinutes());
             var seconds = TK.preZero(date.getSeconds());
-            return date.getFullYear() + '-' + month + '-' + date +' '+ hours + ':' + minutes + ':' + seconds;
+            return date.getFullYear() + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds;
         },
-        localDate : function() {
+        localDate: function() {
             var d = new Date();
-            return d.toLocaleDateString() + ' '+ d.toLocaleTimeString();
+            return d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
         },
-        rand : function(bit) {
+        rand: function(bit) {
             return Math.random() * (10 ^ bit);
         },
         /**
@@ -2561,7 +2387,6 @@ if (typeof TK == 'undefined') {
             ctx.fillRect(x, y, w, h);
             return canvas;
         },
-     
         /**
          * 创建基于canvas 元素的趋势图表 
          * 
