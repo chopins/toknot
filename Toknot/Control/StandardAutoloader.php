4bacb69ee6c6ae115a16d4acfed32f8c06d903a9
chopin <chopins.xiao@gmail.com>
2013-06-28 19:13:17 +0800

    add StandardAutoloader class commit

--- Toknot/Control/StandardAutoloader.php
+++ Toknot/Control/StandardAutoloader.php
@@ -12,69 +12,109 @@ namespace Toknot\Control;
 
 class StandardAutoloader {
 
-    const NS_SEPARATOR = '\\';
+	const NS_SEPARATOR = '\\';
 
-    private $directory = array();
+	private $directory = array();
 
-    public function __construct($path = '') {
-        if ($path == '') {
-            $path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
-        }
-        $this->directory[] = $path;
-    }
+	public function __construct($path = '') {
+		if ($path == '') {
+			$path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
+		}
+		$this->directory[] = $path;
+	}
 
-    public function addPath($path) {
-        $this->directory[] = $path;
-    }
+	/**
+	 * add autoload scan directory
+	 * 
+	 * @param string $path
+	 * @access public
+	 */
+	public function addPath($path) {
+		$this->directory[] = $path;
+	}
 
-    public static function transformClassNameToFilename($class, $dir) {
+	/**
+	 * trans form class name to file name that relative a specified directory
+	 * 
+	 * @param string $class
+	 * @param string $dir
+	 * @return string
+	 * @static
+	 * @access public
+	 */
+	public static function transformClassNameToFilename($class, $dir) {
 //        $topNamespace = strtok($class, self::NS_SEPARATOR);
 //        $lastPath = basename($dir);
 //        if ($topNamespace != $lastPath)
 //            return false;
-        $dir = dirname($dir);
-        $nsPath = strtr($class, self::NS_SEPARATOR, DIRECTORY_SEPARATOR);
-        return $dir . DIRECTORY_SEPARATOR . $nsPath . '.php';
-    }
+		$dir = dirname($dir);
+		$nsPath = strtr($class, self::NS_SEPARATOR, DIRECTORY_SEPARATOR);
+		return $dir . DIRECTORY_SEPARATOR . $nsPath . '.php';
+	}
 
-    public function autoload($class) {
-        foreach ($this->directory as $dir) {
-            $filename = self::transformClassNameToFilename($class, $dir);
-            if (!$filename)
-                continue;
-            //$resolvedName = stream_resolve_include_path($filename);
-            if (file_exists($filename)) {
-                return require_once $filename;
-            }
-        }
-        return false;
-    }
+	/**
+	 * load a class, the method is PHP autoload handler function
+	 * 
+	 * @param string $class
+	 * @return boolean
+	 */
+	public function autoload($class) {
+		foreach ($this->directory as $dir) {
+			$filename = self::transformClassNameToFilename($class, $dir);
+			if (!$filename)
+				continue;
+			//$resolvedName = stream_resolve_include_path($filename);
+			if (file_exists($filename)) {
+				return require_once $filename;
+			}
+		}
+		return false;
+	}
 
-    public static function importToknotClass($class) {
-        $toknotRoot = dirname(__DIR__);
-        $path = $toknotRoot . DIRECTORY_SEPARATOR . strtr($class, self::NS_SEPARATOR, DIRECTORY_SEPARATOR);
-        return require_once $path . '.php';
-    }
+	/**
+	 * manually import one class of toknot instead autoload
+	 * 
+	 * @param string $class
+	 * @return boolean
+	 * @access public
+	 * @static
+	 */
+	public static function importToknotClass($class) {
+		$toknotRoot = dirname(__DIR__);
+		$path = $toknotRoot . DIRECTORY_SEPARATOR . strtr($class, self::NS_SEPARATOR, DIRECTORY_SEPARATOR);
+		return require_once $path . '.php';
+	}
 
-    public static function importToknotModule($module, $first =null) {
-        $toknotRoot = dirname(__DIR__);
-        $path = $toknotRoot . '/' . $module;
-        $dir = dir($path);
-        if($first) {
-            include_once  "{$path}/{$first}.php";
-        }
-        while (false !== ($file = $dir->read())) {
-            if ($file == '.' || $file == '..'|| $file == "{$first}.php") {
-                continue;
-            }
-            if (is_file($path . '/' . $file)) {
-                include_once  $path . '/' . $file;
-            }
-        }
-    }
+	/**
+	 * import under namespace all class of toknot
+	 * 
+	 * @param string $module
+	 * @param string $first
+	 * @access public
+	 * @static
+	 */
+	public static function importToknotModule($module, $first = null) {
+		$toknotRoot = dirname(__DIR__);
+		$path = $toknotRoot . '/' . $module;
+		$dir = dir($path);
+		if ($first) {
+			include_once "{$path}/{$first}.php";
+		}
+		while (false !== ($file = $dir->read())) {
+			if ($file == '.' || $file == '..' || $file == "{$first}.php") {
+				continue;
+			}
+			if (is_file($path . '/' . $file)) {
+				include_once $path . '/' . $file;
+			}
+		}
+	}
 
-    public function register() {
-        spl_autoload_register(array($this, 'autoload'));
-    }
+	/**
+	 * register a autoload handler function
+	 */
+	public function register() {
+		spl_autoload_register(array($this, 'autoload'));
+	}
 
 }
