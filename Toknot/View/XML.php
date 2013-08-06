09ddded09e7433c3d8d432ffac707a41819586a9
chopin <chopins.xiao@gmail.com>
2013-08-06 19:08:03 +0800

    add xml create view

--- Toknot/View/XML.php
+++ Toknot/View/XML.php
@@ -7,11 +7,118 @@
  * @license    http://toknot.com/LICENSE.txt New BSD License
  * @link       https://github.com/chopins/toknot
  */
+
 namespace Toknot\View;
+
 use Toknot\Di\Object;
+use \SplFixedArray;
+use Toknot\Exception\StandardException;
 
 class XML extends Object {
-    public static function singleton() {
-        parent::__singleton();
-    }
+
+	private $encode = 'utf-8';
+
+	public static function singleton() {
+		parent::__singleton();
+	}
+
+	public function setEncoding($encode = 'utf=8') {
+		$this->encode = $encode;
+	}
+
+	public function parseFile($file, $compliefile) {
+		$fileSize = filesize($file);
+		$expectLineNumber = $fileSize / 40;
+		$fp = fopen($file, 'r');
+		$writeFile = fopen($compliefile, 'w');
+		$i = 0;
+		$xml = "<?xml version=\"1.0\" encoding==\"{$this->encode}\" ?>";
+		fwrite($writeFile, $xml, strlen($xml));
+		$indentArray = new SplFixedArray($expectLineNumber);
+		$readSize = 0;
+		if ($fp) {
+			while (($line = fgets($fp)) !== false) {
+				$lineSize = strlen($line);
+				$readSize += $lineSize;
+				if(substr(trim($line),0,1) == '{') {
+					$startControl = $line;
+					continue;
+				}
+				$indentArray[$i] = array();
+				if($startControl !== null) {
+					$indentArray[$i]['control'] = $startControl;
+					$startControl = null;
+				}
+				list($tagName, $value) = explode(trim($line), ' ', 2);
+				if ($i === 0) {
+					
+					$xml = "<$tagName>$value";
+					fwrite($writeFile, $xml, strlen($xml));
+					$indentArray[$i]['indent'] = 0;
+					$indentArray[$i]['tag'] = $tagName;
+				} else {
+
+					$indentArray[$i]['tag'] = $tagName;
+					$indentArray[$i]['indent'] = strlen($line) - strlen(ltrim($line));
+					if ($indentArray[$i]['indent'] % $indentArray[1]['indent'] !== 0) {
+						throw new StandardException('XML template indent error in line ' . $i + 1);
+					}
+					if ($indentArray[$i]['indent'] >= $indentArray[$i - 1]['indent']) {
+						$j = 0;
+						$xml = '';
+						while (true) {
+							$j++;
+							$key = $i - $j;
+							if ($key < 0) {
+								break;
+							}
+							if ($indentArray[$key] === 0) {
+								continue;
+							}
+							if ($indentArray[$i]['indent'] >= $indentArray[$key]['indent']) {
+								$xml .= "</{$indentArray[$key]['tag']}>";
+								$indentArray[$key] = 0;
+							} else {
+								break;
+							}
+						}
+						$xml .= "<$tagName>$value";
+						fwrite($writeFile, $xml, strlen($xml));
+					}
+				}
+				if ($readSize >= $fileSize) {
+					break;
+				}
+				$i++;
+			}
+			$j = 0;
+			$size = $indentArray->getSize();
+			$xml  = '';
+			while (true) {
+				$j++;
+				$key = $size - $j;
+				if ($key < 0) {
+					break;
+				}
+				if ($indentArray[$key] === 0) {
+					continue;
+				}
+				$xml .= "</{$indentArray[$key]['tag']}>";
+			}
+		}
+	}
+
+	/**
+	  TAG	value
+		TAG1 value
+			{foreach }
+			TAG2 value
+	 *		{/foreach}
+			TAG3 value
+		
+
+
+
+	  <TAG>value<TAG1>value<TAG2>value</TAG2>
+	 */
 }
--- test.php
+++ test.php
@@ -1,18 +1,22 @@
 <?php
 
+// Create a new socket
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-$s = microtime(true);
+// An example list of IP addresses owned by the computer
+$sourceips['kevin']    = '127.0.0.1';
+$sourceips['madcoder'] = '127.0.0.2';
 
-for($i=0;$i<1000;$i++) {
-	$ref = new ReflectionExtension('standard');
-	$ref->getFunctions();
-}
-var_dump(microtime(true) - $s);
+// Bind the source address
+socket_bind($sock, '0.0.0.0', 32902);
 
-$s = microtime(true);
+// Connect to destination address
+socket_connect($sock, '127.0.0.1', 80);
 
-for($i=0;$i<1000;$i++) {
-	get_extension_funcs('standard');
-
-}
-var_dump(microtime(true) - $s);
+// Write
+$request = 'GET / HTTP/1.1' . "\r\n" .
+           'Host: example.com' . "\r\n\r\n";
+socket_write($sock, $request);
+sleep(10);
+// Close
+socket_close($sock);
