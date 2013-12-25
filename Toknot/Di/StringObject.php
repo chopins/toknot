1d8c47098fa242a27d6f899d8ebe345d802864b9
chopin <chopins.xiao@gmail.com>
2013-12-25 17:32:16 +0800

    fix StringObject::rand()

--- Toknot/Di/StringObject.php
+++ Toknot/Di/StringObject.php
@@ -176,22 +176,16 @@ class StringObject extends Object implements ArrayAccess {
         if ($min < 1) {
             throw new StandardException("StringObject::rand() 1 parameter must greater 1, $min given");
         }
-        if ($max > 0) {
-            $len = mt_rand($min, $max);
+        if ($max > 1) {
+            $randlen = mt_rand($min, $max);
         } else {
-            $len = $min;
+            $randlen = $min;
         }
         $char = '0987654321qwertyuiopasdfghjklmnbvcxzQWERTYUIUIOPLKJHGFDSAZXCVBNM';
-        $len = 61;
         if ($all) {
             $char .= '~`!@#$%^&*()_+-={}|[]\\:";\',./<>?';
-            $len = 93;
         }
-        $randStr = '';
-        for ($i = 0; $i < $len; $i++) {
-            $randStr = $char[mt_rand(0, $len)];
-        }
-        return $randStr;
+        return substr(str_shuffle($char),0,$randlen);
     }
 
     public function valueOf() {
