dff562798db21a3877ffbf95c1050715f8b3f453
chopin xiao <chopin@localhost.localdomain>
2015-03-10 16:47:16 +0800

    update

--- demos/AppAdmin/Controller/User/Login.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace AppAdmin\Controller\User;
-
-use Toknot\Lib\Admin\Login as AdminLogin;
-
-class Login extends AdminLogin {
-}
--- demos/AppAdmin/Controller/User/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace AppAdmin\Controller\User;
-use Toknot\Lib\Admin\Login;
-class Logout extends Login {
-	public function GET() {
-		parent::logout();	
-		$this->redirectController('\User\Login');
-	}
-}
