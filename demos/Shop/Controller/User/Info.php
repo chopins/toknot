b99a894a236d20e038d07371746bdb8b13e04c9d
chopin xiao <chopin@localhost.localdomain>
2015-03-11 18:19:52 +0800

    update

---
+++ demos/Shop/Controller/User/Info.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace  Shop\Controller\User;
+
+/**
+ * Description of Info
+ *
+ * @author chopin
+ */
+class Info {
+    public function GET() {
+        //$database = $this->AR->connect();
+        print "hello world info";    
+        
+    }
+}
---
+++ demos/Shop/Controller/User/Safe.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Shop\Controller\User;
+
+/**
+ * Description of Safe
+ *
+ * @author chopin
+ */
+class Safe {
+    public function GET() {
+        //$database = $this->AR->connect();
+        print "hello world safe";    
+        
+    }
+}
