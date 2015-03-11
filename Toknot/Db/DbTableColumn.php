38f766e9fb646eb14d6fb6b0e03dd753efb8f728
chopin xiao <chopin@localhost.localdomain>
2015-03-11 10:21:25 +0800

    更新版权时间

--- Toknot/Db/DbTableColumn.php
+++ Toknot/Db/DbTableColumn.php
@@ -3,7 +3,7 @@
 /**
 * Toknot (http://toknot.com)
 *
-* @copyright Copyright (c) 2011 - 2013 Toknot.com
+* @copyright Copyright (c) 2011 - 2015 Toknot.com
 * @license http://toknot.com/LICENSE.txt New BSD License
 * @link https://github.com/chopins/toknot
 */
--- Toknot/Exception/BadPropertyGetException.php
+++ Toknot/Exception/BadPropertyGetException.php
@@ -3,7 +3,7 @@
 /**
  * Toknot (http://toknot.com)
  *
- * @copyright  Copyright (c) 2011 - 2013 Toknot.com
+ * @copyright  Copyright (c) 2011 - 2015 Toknot.com
  * @license    http://opensource.org/licenses/bsd-license.php New BSD License
  * @link       https://github.com/chopins/toknot
  */
