9b9bc18c8cd8858259b8446186edbe461db7addd
chopins <chopins.xiao@gmail.com>
2017-03-15 10:24:43 +0800

    add license

--- vendor/toknot/Toknot/Boot/Route.php
+++ vendor/toknot/Toknot/Boot/Route.php
@@ -1,9 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Toknot (http://toknot.com)
+ *
+ * @copyright  Copyright (c) 2011 - 2017 Toknot.com
+ * @license    http://toknot.com/LICENSE.txt New BSD License
+ * @link       https://github.com/chopins/toknot
  */
 
 namespace Toknot\Boot;
