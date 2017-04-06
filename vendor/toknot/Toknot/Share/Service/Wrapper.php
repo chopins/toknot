6284e4dcc27d1525372264d62fb959de307261f1
chopins <chopins.xiao@gmail.com>
2017-04-06 14:29:54 +0800

    add wrapper example

---
+++ vendor/toknot/Toknot/Share/Service/Wrapper.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Toknot (http://toknot.com)
+ *
+ * @copyright  Copyright (c) 2011 - 2017 Toknot.com
+ * @license    http://toknot.com/LICENSE.txt New BSD License
+ * @link       https://github.com/chopins/toknot
+ */
+
+namespace Toknot\Share\Service;
+
+use Toknot\Boot\SystemCallWrapper;
+use Toknot\Boot\Object;
+use Toknot\Boot\Tookit;
+
+/**
+ * Wapper
+ *
+ * @author chopin
+ */
+class Wrapper extends Object implements SystemCallWrapper {
+
+    protected $pathInfo = [];
+
+    public static function register($protoName = 'ts') {
+        stream_register_wrapper($protoName, __CLASS__, STREAM_IS_URL);
+    }
+
+    public function call() {
+       
+    }
+
+    public function init($path) {
+        $this->pathInfo = parse_url($path);
+    }
+
+    public static function getInstance() {
+        return self::single();
+    }
+
+    public function stream_stat() {
+        return true;
+    }
+
+    public function stream_open($path) {
+        return true;
+    }
+
+}
