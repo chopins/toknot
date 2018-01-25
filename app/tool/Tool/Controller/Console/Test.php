2646178e7a9669284a1d826360e1939aa171a6c7
chopins <chopins.xiao@gmail.com>
2018-01-25 14:27:12 +0800

    update

--- app/tool/Tool/Controller/Console/Test.php
+++ app/tool/Tool/Controller/Console/Test.php
@@ -37,18 +37,14 @@ class Test {
     public function __construct() {
         $this->kernel = \Toknot\Boot\Kernel::single();
         $this->cmd = new \Toknot\Share\CommandLine;
-        //$this->process = new \Toknot\Share\Process\Process();
+        $this->process = new \Toknot\Share\Process\Process();
         $this->run();
     }
 
     public function run() {
-        echo $this->kernel->getArg(1);
-        echo $this->kernel->getArg('-a');
-        echo $this->kernel->getArg('-b');
-        echo $this->kernel->getArg('--with-fo');
-        echo $this->kernel->getArg('-的');
-                echo $this->kernel->getArg(8);
-
+       $number = new \Toknot\Share\ChineseNumber(5635001243389);
+       $number = new \Toknot\Share\ChineseNumber(19);
+       $this->cmd->message($number);
     }
 
     public function loop() {
